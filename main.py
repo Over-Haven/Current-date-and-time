7f184c46522a0a5e85ab018afacf07497f2adb38
Over-haven <tanushdas26@gmail.com>
2024-12-08 20:27:13 +0530

    Date

---
+++ main.py
@@ -0,0 +1,7 @@
+from datetime import date , time , datetime
+
+today = date.today()
+now = datetime.now()
+print("\ncurrent date and time is", now)
+
+print("\n Date cpmponents", today.year, today.month, today.day)
